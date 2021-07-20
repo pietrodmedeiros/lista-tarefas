f4ca90290bd6d85b07086baa233d703a264930b4
Pietro Medeiros <spogmedeiros@gmail.com>
2021-07-20 15:58:19 -0300

    deleting tasks on local storage and screen

--- scripts.js
+++ scripts.js
@@ -25,28 +25,37 @@ const Main = {
         this.inputTask.onkeypress = self.Events.inputTask_keyPress.bind(this)
 
         this.removeButtons.forEach(function(button){
-            button.onclick = self.Events.deleteButton_click
+            button.onclick = self.Events.deleteButton_click.bind(self)
         })
     },
 
     getLocalStorage: function() {
         const tasks = localStorage.getItem('tasks')
-        this.tasks = JSON.parse(tasks)
+
+        if (tasks) {
+            this.tasks = JSON.parse(tasks)
+        } else {
+            localStorage.setItem('tasks', JSON.stringify([]))
+        }
     },
 
+
+    getTaskHtml: function(task){
+        return `
+            <li id="done">
+                <div class="check"></div>
+                <label class="task">
+                    ${task}
+                </label>
+                <img class="remove" data-task="${task}" src="./img/delete.svg">
+            </li>
+            `
+    },
     showTasks: function(){
         let html = ''
         this.tasks.forEach(task =>{
-            html += 
-            `
-                <li id="done">
-                    <div class="check"></div>
-                    <label class="task">
-                        ${task.task}
-                    </label>
-                    <img class="remove" src="./img/delete.svg">
-                </li>
-            `
+            html += this.getTaskHtml(task.task)
+            
         })
 
         this.list.innerHTML = html
@@ -69,41 +78,60 @@ const Main = {
 
         deleteButton_click: function(e){
             const li = e.target.parentElement
+            const value = e.target.dataset['task']
+
+            const newTasks = this.tasks.filter(task => task.task !== value)
+            localStorage.setItem('tasks', JSON.stringify(newTasks))
+            this.tasks = newTasks
+
+
             li.classList.add("hidden1")
             setTimeout(function(){
                 li.classList.add("hidden2")
             },300)
+
         },
+
+/*         deleteAllTasksButton_click: function(e){
+            const li = e.target.parentElement
+            const value = e.target.dataset['task']
+            
+            EmptyTasks = []
+            const jsonEmptyTasks = JSON.stringify(EmptyTasks)
+
+            localStorage.setItem('tasks', jsonEmptyTasks)
+            this.tasks = EmptyTasks
+            li.classList.add("hidden1")
+            setTimeout(function(){
+                li.classList.add("hidden2")
+            },300)
+            
+            this.showTasks(EmptyTasks)
+        }, */
 
         inputTask_keyPress: function(e){
             const key = e.key
             const newTask = e.target.value
 
             if (key === 'Enter'){
-                this.list.innerHTML += 
-                `
-                    <li id="done">
-                        <div class="check"></div>
-                            <label class="task">
-                                ${newTask}
-                            </label>
-                        <img class="remove" src="./img/delete.svg">
-                    </li>
-                `
+                this.list.innerHTML += this.getTaskHtml(newTask)
+                
                 e.target.value = ''
                 this.selectors()
                 this.callEvents()
 
                 const savedTasks = localStorage.getItem('tasks')
-                const savedTasksObj = JSON.parse(savedTasks)
+                const savedTasksArr = JSON.parse(savedTasks)
 
 
-                const obj = [
+                const arrTasks = [
                     { task: newTask },
-                    ...savedTasksObj,
+                    ...savedTasksArr,
                 ]
-
-                localStorage.setItem('tasks', JSON.stringify(obj))
+                
+                const jsonTasks = JSON.stringify(arrTasks)
+                this.tasks = arrTasks
+                localStorage.setItem('tasks', jsonTasks)
             }
         }
     }
